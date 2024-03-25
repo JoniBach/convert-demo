160caa7b4b29090ff231ee025632344e718e098c
James <james.crook@fourthfloor.co>
2024-03-25 21:17:22 +0000

    adding arrows

--- src/lib/components/BasicConverter.svelte
+++ src/lib/components/BasicConverter.svelte
@@ -43,15 +43,68 @@
 {#if loading}
 	<p>Loading...</p>
 {:else if readOnly}
-	<div>
-		{displayValue}
-		{from} = {displayConvertedValue}
-		{to}
+	<div class="display">
+		<p class="from">{displayValue}</p>
+		<div class="break">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+				/>
+			</svg>
+		</div>
+		<p class="from">{displayConvertedValue}</p>
 	</div>
 {:else}
-	<div>
-		<input bind:value />
-		{from} = {convertedValue}
-		{to}
+	<div class="display">
+		<input class="from" bind:value />
+		<div class="break">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+				/>
+			</svg>
+		</div>
+		<div class="to">{displayConvertedValue}</div>
 	</div>
 {/if}
+
+<style>
+	.break {
+		width: 20px;
+		height: 20px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: auto;
+		padding: 5px;
+	}
+	.display {
+		display: flex;
+	}
+
+	.from {
+		padding: 10px;
+	}
+
+	.to {
+		padding: 10px;
+	}
+</style>
